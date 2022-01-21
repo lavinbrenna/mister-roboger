6674076cb8628b346b8dadbdfdb1695d01fb4f93
Brenna Lavin <lavinbrenna@gmail.com>
2022-01-21 08:45:42 -0800

    refactor code, pass second test

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,12 @@
-let userInput = 0;
+let userInput = "10";
 
 function numberAnalyzer(input){
-  let numbers = [0,1,2,3,4,5,6,7,8,9];
-  for(i = 0; i < numbers.length; i ++){
-    if(input === numbers[i]){
-      return true;
-    }
-    else{
-      return false;
-    }
+  let numbers = /^[0-9]+$/;
+  if(input.match(numbers) != null){
+    return true;
+  }
+  else{
+    return false;
   }
 }
 
