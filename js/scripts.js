4b12a9d80ddcff0e0bacb1232f6891435b188035
Brenna Lavin <lavinbrenna@gmail.com>
2022-01-21 13:00:35 -0800

    update code to change message based off of included numbers

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,7 @@
 //Business Logic
-let userInput = "13";
-
-function numberAnalyzer(input){
+function numberAnalyzer(number){
   let numbers = /^[0-9]+$/;
-  if(input.match(numbers) != null){
+  if(number.match(numbers) != null){
     return true;
   }
   else{
@@ -11,40 +9,75 @@ function numberAnalyzer(input){
   }
 }
 
-function messageCreator(input){
-  let userNumber = parseInt(input);
+function messageCreator(number, name){
+  let userNumber = parseInt(number);
   const userArray = [];
   for(i = 0; i <= userNumber; i++){
-    if(i === 1){
-      userArray.push("Beep!");
-    }else if(i === 2){
-      userArray.push("Boop!");
-    }else if(i === 3){
-      userArray.push("Won't you be my neighbor?")
-    }else if(i === 13){
-      userArray.push("Won't you be my neighbor?")
-    }else if(i === 21){
-      userArray[13] = 13;
-      userArray.push("Boop");
-    }else if(i === 32){
-      userArray[13] = 13;
-      userArray[21] = 21;
-      userArray.push("Won't you be my neighbor?");
+    if(i <= 9){
+      if(i === 1){
+        userArray.push("Beep!");
+      }else if(i === 2){
+        userArray.push("Boop!");
+      }else if(i === 3){
+        userArray.push(name + ", won't you be my neighbor?");
+      }else{
+        userArray.push(i);
+      }
+      }
+    else if(i > 9){
+      let newDigit = i.toString().split('');
+      if (newDigit[0].includes(1) && (newDigit[1] != '2' && newDigit[1] != '3')){
+        userArray.push("Beep!");
+      }
     }
-    else{
-      userArray.push(i);
     }
+    /*else if(i > 9){
+      let newDigit = i.toString().split('');
+      if((newDigit[0].includes(1))&& (newDigit[1] != '2' && newDigit[1] != '3')){
+        userArray.push("Beep!");
+      }
+      else if((newDigit[0].includes(1) && newDigit[1].includes(2))){
+        console.log(newDigit[0],newDigit[1]);
+        userArray.push("Boop!");
+      }
+      else if((newDigit[0].includes(1) && newDigit[1].includes(3))){
+        console.log(newDigit[0], newDigit[1]);
+        userArray.push(name + ", won't you be my neighbor?");
+      }
+      else if((newDigit[0].includes(2) && newDigit[1]!= '3')){
+        userArray.push("Boop!");
+      }
+      else if(newDigit[0].includes(2)&& newDigit[1].includes(3)){
+        userArray.push(name + ", won't you be my neighbor?");
+      }
+      else if(newDigit[0].includes(3)){
+        userArray.push(name + ", won't you be my neighbor?");
+      }
+      else if(newDigit[1].includes(1)){
+          userArray.push("Beep!");
+        }
+      else if(newDigit[1].includes(2)){
+          userArray.push("Boop!");
+        }
+      else if(newDigit[1].includes(3)){
+          userArray.push(name + ", won't you be my neighbor?");
+        }else{
+          userArray.push(i);
+        }
+      }*/
+      return userArray.join(", ");
   }
-  return userArray.join(", ");
-}
+  
 
 //UI Logic
+
 $(document).ready(function(){
   $("form#messageForm").submit(function(){
     event.preventDefault();
     const userInput = $("input#userInput").val();
+    const userName = $("input#userName").val();
     const isNumber = numberAnalyzer(userInput);
-    const userMessage = messageCreator(userInput);
+    const userMessage = messageCreator(userInput, userName);
     if(isNumber === true){
       $("#response").show();
       $("#error").hide();
